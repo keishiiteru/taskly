6fc1f3bcc35b587a1baffd2c1dada68c314f1726
Kaycee Lim <kasslim749@gmail.com>
2026-06-10 07:15:11 +0800

    Laravel API route initialization. Configure axios for API requests

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -1,3 +1,15 @@
 <template>
     <h1>Welcome to Dashboard!</h1>
+      <pre>{{ result }}</pre>
 </template>
+
+<script setup>
+    import { onMounted, ref } from 'vue'
+
+    const result = ref(null)
+
+    onMounted(async () => {
+    const { data } = await axios.get('/api/test')
+    result.value = data
+    })
+</script>
